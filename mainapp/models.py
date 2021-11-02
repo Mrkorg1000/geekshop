d58d93b01871e0398423c8e23f67585c769aa561
korg1000 <Mrkorg1000>
2021-11-03 01:21:47 +0300

    Lesson 3. Code style fix

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class ProductCategory(models.Model):
     name = models.CharField(verbose_name="имя", max_length=64, unique=True)
@@ -7,6 +8,7 @@ class ProductCategory(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
